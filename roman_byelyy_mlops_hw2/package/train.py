9195dd6c564674500a6d91406b056ebf503bcef7
Roman Belyy <rbelyy@aligntech.com>
2023-12-24 01:41:29 +0300

    add data set

--- roman_byelyy_mlops_hw2/package/train.py
+++ roman_byelyy_mlops_hw2/package/train.py
@@ -1,17 +1,13 @@
-# train.py
-import pickle
-
 import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import fetch_california_housing
+import pickle
 
 class TrainModel:
     def __init__(self):
-        iris = load_iris()
-        self.data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-        self.data["target"] = iris.target
+        california_housing = fetch_california_housing(as_frame=True)
+        self.data = california_housing.frame
         self.model = RandomForestClassifier()
 
     def preprocess_data(self):
@@ -19,20 +15,17 @@ class TrainModel:
         pass
 
     def train(self):
-        X = self.data.drop("target", axis=1)
-        y = self.data["target"]
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
-        )
+        X = self.data.drop('MedHouseVal', axis=1)
+        y = self.data['MedHouseVal']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         self.model.fit(X_train, y_train)
         print(f"Model accuracy: {self.model.score(X_test, y_test)}")
 
     def save_model(self):
-        with open("model.pkl", "wb") as f:
+        with open('model.pkl', 'wb') as f:
             pickle.dump(self.model, f)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     model = TrainModel()
     model.preprocess_data()
     model.train()
