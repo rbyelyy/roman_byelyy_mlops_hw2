4f666aa84c8d659a73c3d11f62d2976a8ba48841
Roman Belyy <rbelyy@aligntech.com>
2023-12-24 00:51:06 +0300

    added model Iris

---
+++ roman_byelyy_mlops_hw2/package/infer.py
@@ -0,0 +1,23 @@
+# infer.py
+import pandas as pd
+import pickle
+from sklearn.datasets import load_iris
+
+class InferModel:
+    def __init__(self, model_file):
+        with open(model_file, 'rb') as f:
+            self.model = pickle.load(f)
+        iris = load_iris()
+        self.data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+    def predict(self):
+        predictions = self.model.predict(self.data)
+        return predictions
+
+    def save_predictions(self, predictions):
+        pd.DataFrame(predictions).to_csv('predictions.csv', index=False)
+
+if __name__ == '__main__':
+    infer = InferModel('model.pkl')
+    predictions = infer.predict()
+    infer.save_predictions(predictions)
