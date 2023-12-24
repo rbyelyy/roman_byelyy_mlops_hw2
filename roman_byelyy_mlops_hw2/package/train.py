63139d8faeceeee40608aad9756fd8f8d616451d
Roman Belyy <rbelyy@aligntech.com>
2023-12-24 11:46:12 +0300

    remove ignored.txt

--- roman_byelyy_mlops_hw2/package/train.py
+++ roman_byelyy_mlops_hw2/package/train.py
@@ -1,8 +1,10 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import fetch_california_housing
 import pickle
+
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
 
 class TrainModel:
     def __init__(self):
@@ -14,18 +16,25 @@ class TrainModel:
         # Здесь вы можете добавить код для предварительной обработки данных
         pass
 
+    def dvc_download(self, file):
+        # скачать файл
+        pass
+    
     def train(self):
-        X = self.data.drop('MedHouseVal', axis=1)
-        y = self.data['MedHouseVal']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X = self.data.drop("MedHouseVal", axis=1)
+        y = self.data["MedHouseVal"]
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
         self.model.fit(X_train, y_train)
         print(f"Model accuracy: {self.model.score(X_test, y_test)}")
 
     def save_model(self):
-        with open('model.pkl', 'wb') as f:
+        with open("model.pkl", "wb") as f:
             pickle.dump(self.model, f)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     model = TrainModel()
     model.preprocess_data()
     model.train()
