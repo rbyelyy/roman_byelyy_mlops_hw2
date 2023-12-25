7a262461c9fc4fbf948798237108561a9ec9823c
Roman Belyy <rbelyy@aligntech.com>
2023-12-25 23:17:13 +0300

    add charts to train

--- roman_byelyy_mlops_hw2/package/infer.py
+++ roman_byelyy_mlops_hw2/package/infer.py
@@ -1,26 +1,187 @@
 # infer.py
+"""
+Performing data downloading from DVC and applying model on a test data
+"""
+import os
 import pickle
 
 import pandas as pd
-from sklearn.datasets import load_iris
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+import roman_byelyy_mlops_hw2.package.utils as ut
 
 
 class InferModel:
-    def __init__(self, model_file):
-        with open(model_file, "rb") as f:
-            self.model = pickle.load(f)
-        iris = load_iris()
-        self.data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    """
+    Main class for making an inference for the model
+    """
+
+    def __init__(self):
+        """
+        Constructor for the InferModel class. Initializes all
+        necessary attributes.
+        """
+        self.target_scaler = None  # Scaler for the target variable
+        self.model = None  # The model we will use for predictions
+        self.data = None  # The data we will make predictions on
+        self.scaler = None  # Scaler for the features
+        self.original_data = None  # Original data before preprocessing
+
+    def load_data(self):
+        """
+        Loads the data for prediction. The data is loaded
+        using DVC and OS utilities.
+        """
+        # Initialize DVC and OS utility instances
+        dvc_instance = ut.DVCUtils()
+        os_instance = ut.OSUtils()
+
+        # Construct the file path
+        # file_path = os_instance.test_file
+        file_path = ut.OSUtils.build_file_path(os_instance.test_file)
+
+        # Set up a DVC remote and pull the data
+        remote_name = dvc_instance.remote_name
+        remote_url = dvc_instance.remote_url
+        dvc_instance.add_remote_dvc(remote_name=remote_name, remote_url=remote_url)
+        dvc_instance.pull_dvc()
+
+        # Check if the file exists
+        if os.path.isfile(file_path):
+            print(f"File is present: {file_path}")
+            # If the file exists, load it into a DataFrame
+            self.data = os_instance.import_csv_to_dataframe(file_path)
+            self.original_data = self.data.copy()
+            target = self.data["median_house_value"]
+            self.target_scaler = StandardScaler()
+            self.target_scaler.fit_transform(target.values.reshape(-1, 1))
+            self.data = self.data.drop("median_house_value", axis=1)
+        else:
+            print(f"File is not present: {file_path}")
+
+    def preprocess_data(self) -> None:
+        """
+        Preprocesses the data. This includes one-hot encoding
+        categorical features, imputing missing values,
+        and scaling numerical features.
+        """
+        # One-hot encode categorical features
+        self.data = pd.get_dummies(self.data, columns=["ocean_proximity"])
+        columns = self.data.columns
+
+        # Create an instance of SimpleImputer with the
+        # strategy defined in the Hydra config
+        impute = SimpleImputer()
+
+        # Apply SimpleImputer to the DataFrame
+        self.data = pd.DataFrame(
+            impute.fit_transform(self.data), columns=self.data.columns
+        )
+
+        # Create an instance of StandardScaler with
+        # the parameters defined in the Hydra config
+        self.scaler = StandardScaler()
+
+        # Apply StandardScaler to the DataFrame
+        df_scaled = self.scaler.fit_transform(self.data)
+
+        # Convert the NumPy array back to a DataFrame
+        self.data = pd.DataFrame(df_scaled, columns=columns)
+
+    def load_model(self, model_name):
+        """
+        Loads the model from a .pkl file.
+
+        Args:
+            model_name (str): The name of the model
+            file without the extension.
+        """
+        # Specify the path to your model file
+        model_file = f"{model_name}.pkl"
+
+        # Load the model from the file
+        with open(model_file, "rb") as file:
+            self.model = pickle.load(file)
 
     def predict(self):
+        """
+        Makes predictions based on the loaded model and data.
+
+        Returns:
+            numpy.ndarray: An array with the model's predictions.
+        """
         predictions = self.model.predict(self.data)
         return predictions
 
-    def save_predictions(self, predictions):
-        pd.DataFrame(predictions).to_csv("predictions.csv", index=False)
+    def inverse_scale_predictions(self, predictions):
+        """
+        Transforms the predictions back to the original scale.
+
+        Args:
+            predictions (numpy.ndarray): An array with
+            the model's predictions.
+
+        Returns:
+            numpy.ndarray: An array with the predictions
+            transformed back to the original scale.
+        """
+        predictions = predictions.reshape(-1, 1)
+        return self.target_scaler.inverse_transform(predictions)
+
+    @staticmethod
+    def save_predictions(result, filename):
+        """
+        Saves the predictions to a .csv file.
+
+        Args:
+            result (pandas.DataFrame): A DataFrame
+            with the predictions.
+            filename (str): The name of the file
+            where the predictions will be saved.
+        """
+        result.to_csv(filename, index=False)
+
+
+def main() -> None:
+    """
+    Main entry point of the script.
+
+    This function creates an instance of the TrainModel
+    class and performs data loading, preprocessing, model training,
+    and model saving.
+
+    Raises:
+        ValueError: If the data is not loaded or
+        preprocessed before training.
+    """
+    # Create an instance of TrainModel with the Hydra configuration
+    model = InferModel()
+
+    # Load the data
+    model.load_data()
+
+    # Load the model
+    model.load_model(model_name="model")
+
+    # Preprocess the data
+    model.preprocess_data()
+
+    # Make predictions
+    predictions = model.predict()
+
+    # Inverse scale the predictions
+    predictions = model.inverse_scale_predictions(predictions)
+
+    # Convert predictions to a DataFrame
+    predictions_df = pd.DataFrame(predictions, columns=["Predictions"])
+
+    # Concatenate the original data with the predictions
+    result = pd.concat([model.original_data, predictions_df], axis=1)
+
+    # Save the predictions to a csv file
+    model.save_predictions(result, "predictions.csv")
 
 
 if __name__ == "__main__":
-    infer = InferModel("model.pkl")
-    predictions = infer.predict()
-    infer.save_predictions(predictions)
+    main()
--- roman_byelyy_mlops_hw2/package/train.py
+++ roman_byelyy_mlops_hw2/package/train.py
@@ -1,86 +1,107 @@
+"""
+Train Model
+"""
 import os
 import pickle
-import utils
+
+import git
+import hydra
+import matplotlib.pyplot as plt
+import mlflow
 import pandas as pd
+from omegaconf import DictConfig
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
+from torch.utils.tensorboard import SummaryWriter
 
-from omegaconf import DictConfig, OmegaConf
-import hydra
+import roman_byelyy_mlops_hw2.package.utils as ut
+
 
 class TrainModel:
-    def __init__(self, cfg):
-        self.cfg = cfg
-        self.data = None
-        self.model = RandomForestRegressor(n_estimators=self.cfg.model.n_estimators,
-                                           random_state=self.cfg.model.random_state,
-                                           max_depth=self.cfg.model.max_depth,
-                                           max_features=self.cfg.model.max_features)
-        self.os_utils = utils.OSUtils()
+    """
+    Main class for model training and data downloading
+    """
 
-    @staticmethod
-    def import_csv_to_dataframe(file_path):
+    def __init__(self, cfg):
         """
-        Import a CSV file and convert it to a pandas DataFrame.
+        Initialize the TrainModel class with the given configuration.
 
         Args:
-            file_path (str): The path to the CSV file.
-
-        Returns:
-            df (pd.DataFrame): The loaded data as a pandas DataFrame.
+            cfg: A configuration object containing parameters for the model and preprocessing steps.
         """
-        df = pd.read_csv(file_path)
-        return df
+        self.cfg = cfg
+        self.data = None
+        self.model = RandomForestRegressor(
+            n_estimators=self.cfg.model.n_estimators,
+            random_state=self.cfg.model.random_state,
+            max_depth=self.cfg.model.max_depth,
+            max_features=self.cfg.model.max_features,
+        )
+        self.os_utils = ut.OSUtils()
 
     def load_data(self):
         """
-        Load the training data using DVC and OS utilities. The data is expected to be in CSV format.
+        Load the training data using DVC and OS utilities. The
+        data is expected to be in CSV format.
 
-        The method first checks if the file exists at the specified path. If the file exists, it is loaded into a
-        DataFrame.
+        The method first checks if the file exists at the specified path.
+        If the file exists, it is loaded into a DataFrame.
         If the file does not exist, a message is printed to the console.
 
-        The method also sets up a DVC remote and pulls the data from the remote if necessary.
+        The method also sets up a DVC remote and pulls the data
+        from the remote if necessary.
         """
         # Initialize DVC and OS utility instances
-        dvc_instance = utils.DVCUtils()
-        os_instance = utils.OSUtils()
+        dvc_instance = ut.DVCUtils()
+        os_instance = ut.OSUtils()
 
         # Construct the file path
-        file_path = os_instance.train_file
+        file_path = ut.OSUtils.build_file_path(os_instance.train_file)
 
         # Set up a DVC remote and pull the data
-        dvc_instance.add_remote_dvc(remote_name=dvc_instance.remote_name, remote_url=dvc_instance.remote_url)
+        remote_name = dvc_instance.remote_name
+        remote_url = dvc_instance.remote_url
+        dvc_instance.add_remote_dvc(remote_name=remote_name, remote_url=remote_url)
         dvc_instance.pull_dvc()
 
         # Check if the file exists
         if os.path.isfile(file_path):
-            print(f'File is present: {file_path}')
+            print(f"Nice file for training is present: {file_path}")
             # If the file exists, load it into a DataFrame
-            self.data = self.import_csv_to_dataframe(file_path)
+            self.data = os_instance.import_csv_to_dataframe(file_path)
         else:
-            print(f'File is not present: {file_path}')
+            print(f"Hm...I cannot find file: {file_path}")
+            exit("if no training file then no prediction :-(")
 
     def preprocess_data(self) -> None:
         """
-        Preprocess the data using the parameters defined in the Hydra configuration.
-        This includes one-hot encoding categorical features, imputing missing values,
+        Preprocess the data using the parameters defined
+        in the Hydra configuration.
+        This includes one-hot encoding categorical
+        features, imputing missing values,
         and scaling numerical features.
         """
         # One-hot encode categorical features
-        self.data = pd.get_dummies(self.data, columns=['ocean_proximity'])
+        self.data = pd.get_dummies(self.data, columns=["ocean_proximity"])
         columns = self.data.columns
 
-        # Create an instance of SimpleImputer with the strategy defined in the Hydra config
+        # Create an instance of SimpleImputer with
+        # the strategy defined in the Hydra config
         impute = SimpleImputer(strategy=self.cfg.imputer.strategy)
 
         # Apply SimpleImputer to the DataFrame
-        self.data = pd.DataFrame(impute.fit_transform(self.data), columns=self.data.columns)
+        self.data = pd.DataFrame(
+            impute.fit_transform(self.data), columns=self.data.columns
+        )
 
-        # Create an instance of StandardScaler with the parameters defined in the Hydra config
-        scaler = StandardScaler(with_mean=self.cfg.scaler.with_mean, with_std=self.cfg.scaler.with_std)
+        # Create an instance of StandardScaler with
+        # the parameters defined in the Hydra config
+        with_mean = self.cfg.scaler.with_mean
+        with_std = self.cfg.scaler.with_std
+        scaler = StandardScaler(with_mean=with_mean, with_std=with_std)
 
         # Apply StandardScaler to the DataFrame
         df_scaled = scaler.fit_transform(self.data)
@@ -89,48 +110,119 @@ class TrainModel:
         self.data = pd.DataFrame(df_scaled, columns=columns)
 
     def train(self):
-        """
-        Train the model using the preprocessed data.
+        try:
+            # Get the git commit id
+            repo = git.Repo(search_parent_directories=True)
+            git_commit_id = repo.head.object.hexsha
 
-        The data is split into training and testing sets. The split ratio and random state are defined in the Hydra
-        configuration.
-        The model is then trained using the training set and the accuracy of the model is printed.
+            # Set up MLflow
+            mlflow.set_tracking_uri("http://127.0.0.1:8080")
+            mlflow.set_experiment("roman_byelyy_mlops_hw2")
 
-        Raises:
-            ValueError: If the data is not loaded or preprocessed before training.
-        """
-        # Check if data is loaded and preprocessed
-        if self.data is None:
-            raise ValueError("Data is not loaded or preprocessed.")
+            # Set up TensorBoard
+            writer = SummaryWriter(
+                log_dir="/Users/rbelyy/Downloads/apps/mlflow/tensor_logs"
+            )
 
-        # Separate the features (X) and the target variable (y)
-        X = self.data.drop("median_house_value", axis=1)
-        y = self.data["median_house_value"]
+            with mlflow.start_run(run_name="rbyelyy_run"):
+                # Log hyperparameters and code version
+                mlflow.log_param("test_size", self.cfg.split.test_size)
+                mlflow.log_param("random_state", self.cfg.model.random_state)
+                mlflow.log_param("git_commit_id", git_commit_id)
 
-        # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=self.cfg.split.test_size, random_state=self.cfg.model.random_state
-        )
+                # Check if data is loaded and preprocessed
+                if self.data is None:
+                    raise ValueError("Data is not loaded or preprocessed.")
 
-        # Train the model using the training set
-        self.model.fit(X_train, y_train)
+                # Separate the features (X) and the target variable (y)
+                X = self.data.drop("median_house_value", axis=1)
+                y = self.data["median_house_value"]
 
-        # Print the accuracy of the model on the testing set
-        print(f"Model accuracy: {self.model.score(X_test, y_test)}")
+                # Split the data into training and testing sets
+                X_train, X_test, y_train, y_test = train_test_split(
+                    X,
+                    y,
+                    test_size=self.cfg.split.test_size,
+                    random_state=self.cfg.model.random_state,
+                )
+
+                train_losses = []
+                val_losses = []
+
+                for epoch in range(5):
+                    # Train the model using the training dataset
+                    self.model.fit(X_train, y_train)
+
+                    # Compute MSE
+                    y_pred_train = self.model.predict(X_train)
+                    mse_train = mean_squared_error(y_train, y_pred_train)
+
+                    # Add the MSE value to the list
+                    train_losses.append(mse_train)
+
+                    # Compute MSE on the validation dataset
+                    y_pred_val = self.model.predict(X_test)
+                    mse_val = mean_squared_error(y_test, y_pred_val)
+
+                    # Add the MSE value to the list
+                    val_losses.append(mse_val)
+
+                    # Log MSE in TensorBoard
+                    writer.add_scalar("Loss/train", mse_train, epoch)
+                    writer.add_scalar("Loss/val", mse_val, epoch)
+
+                # Create a plot of training and validation losses
+                fig = plt.figure()
+                plt.plot(range(1, len(train_losses) + 1), train_losses, label="Train")
+                plt.plot(range(1, len(val_losses) + 1), val_losses, label="Val")
+                plt.title("Training and Validation Losses")
+                plt.xlabel("Epoch")
+                plt.ylabel("Loss")
+                plt.legend()
+
+                # Log the plot in TensorBoard
+                writer.add_figure("Loss/plot", fig)
+
+                # Compute MSE on the test dataset
+                y_pred_test = self.model.predict(X_test)
+                mse_test = mean_squared_error(y_test, y_pred_test)
+
+                # Log MSE in MLflow
+                mlflow.log_metric("mse", mse_test)
+
+                # Compute the model's accuracy
+                accuracy = self.model.score(X_test, y_test)
+
+                # Print the model's accuracy
+                print(f"Model accuracy: {accuracy}")
+
+                # Log accuracy in MLflow
+                mlflow.log_metric("accuracy", accuracy)
+
+                # Log the model in MLflow
+                mlflow.sklearn.log_model(self.model, "model")
+
+        except Exception as e:
+            print(f"An error occurred during the training process: {e}")
 
     def save_model(self):
         """
         Save the trained model to a file.
 
-        The model is saved using pickle, which serializes the model object for future use. The filename is defined in
+        The model is saved using pickle, which serializes the
+        model object for future use. The filename is defined in
         the Hydra configuration.
 
         Raises:
-            FileNotFoundError: If the directory specified in the Hydra config does not exist.
+            FileNotFoundError: If the directory specified in the
+            Hydra config does not exist.
         """
         # Save the model to a file
         with open(self.cfg.output.model_file, "wb") as f:
             pickle.dump(self.model, f)
+
+        # Save the model in MLflow
+        mlflow.sklearn.log_model(self.model, "model")
 
 
 @hydra.main(config_path="../configs", config_name="config", version_base="1.2")
@@ -138,8 +230,8 @@ def main(cfg: DictConfig) -> None:
     """
     Main entry point of the script.
 
-    This function creates an instance of the TrainModel class and performs data loading, preprocessing, model training,
-    and model saving.
+    This function creates an instance of the TrainModel class and
+    performs data loading, preprocessing, model training, and model saving.
 
     Args:
         cfg (DictConfig): The Hydra configuration object.
