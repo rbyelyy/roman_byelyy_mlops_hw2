ac3d683b1d3a4d6bd511050edb78a2fa361bf5e3
Roman Belyy <rbelyy@aligntech.com>
2023-12-26 13:02:08 +0300

    added logging

--- roman_byelyy_mlops_hw2/package/infer.py
+++ roman_byelyy_mlops_hw2/package/infer.py
@@ -2,13 +2,19 @@
 """
 Performing data downloading from DVC and applying model on a test data
 """
-import os
-import pickle
 
+# Import necessary libraries
+import logging
+import os
+
+import hydra
 import pandas as pd
+from joblib import load
+from omegaconf import DictConfig
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
+# Import custom utilities
 import roman_byelyy_mlops_hw2.package.utils as ut
 
 
@@ -17,7 +23,7 @@ class InferModel:
     Main class for making an inference for the model
     """
 
-    def __init__(self):
+    def __init__(self, cfg):
         """
         Constructor for the InferModel class. Initializes all
         necessary attributes.
@@ -27,19 +33,23 @@ class InferModel:
         self.data = None  # The data we will make predictions on
         self.scaler = None  # Scaler for the features
         self.original_data = None  # Original data before preprocessing
+        self.cfg = cfg
+        self.filename = os.path.basename(__file__).split(".")[0]  # get filename
+        self.logger = logging.getLogger(self.filename)  # Add a logging instance
 
     def load_data(self):
         """
         Loads the data for prediction. The data is loaded
         using DVC and OS utilities.
         """
+        self.logger.info("Loading data for prediction.")
+
         # Initialize DVC and OS utility instances
         dvc_instance = ut.DVCUtils()
         os_instance = ut.OSUtils()
 
         # Construct the file path
-        # file_path = os_instance.test_file
-        file_path = ut.OSUtils.build_file_path(os_instance.test_file)
+        file_path = os_instance.build_file_path(file_name=os_instance.test_file)
 
         # Set up a DVC remote and pull the data
         remote_name = dvc_instance.remote_name
@@ -49,7 +59,7 @@ class InferModel:
 
         # Check if the file exists
         if os.path.isfile(file_path):
-            print(f"File is present: {file_path}")
+            self.logger.info(f"File is present: {file_path}")
             # If the file exists, load it into a DataFrame
             self.data = os_instance.import_csv_to_dataframe(file_path)
             self.original_data = self.data.copy()
@@ -58,7 +68,7 @@ class InferModel:
             self.target_scaler.fit_transform(target.values.reshape(-1, 1))
             self.data = self.data.drop("median_house_value", axis=1)
         else:
-            print(f"File is not present: {file_path}")
+            self.logger.warning(f"File is not present: {file_path}")
 
     def preprocess_data(self) -> None:
         """
@@ -66,9 +76,13 @@ class InferModel:
         categorical features, imputing missing values,
         and scaling numerical features.
         """
+        self.logger.info("Starting data preprocessing.")
+
         # One-hot encode categorical features
         self.data = pd.get_dummies(self.data, columns=["ocean_proximity"])
         columns = self.data.columns
+
+        self.logger.info("Categorical features have been one-hot encoded.")
 
         # Create an instance of SimpleImputer with the
         # strategy defined in the Hydra config
@@ -79,6 +93,8 @@ class InferModel:
             impute.fit_transform(self.data), columns=self.data.columns
         )
 
+        self.logger.info("Missing values have been imputed.")
+
         # Create an instance of StandardScaler with
         # the parameters defined in the Hydra config
         self.scaler = StandardScaler()
@@ -86,23 +102,32 @@ class InferModel:
         # Apply StandardScaler to the DataFrame
         df_scaled = self.scaler.fit_transform(self.data)
 
+        self.logger.info("Numerical features have been scaled.")
+
         # Convert the NumPy array back to a DataFrame
         self.data = pd.DataFrame(df_scaled, columns=columns)
 
+        self.logger.info("Data preprocessing completed.")
+
     def load_model(self, model_name):
         """
-        Loads the model from a .pkl file.
+        Loads the model from a .joblib file.
 
         Args:
             model_name (str): The name of the model
             file without the extension.
         """
         # Specify the path to your model file
-        model_file = f"{model_name}.pkl"
+        model_file = self.cfg.output.model_file
+
+        self.logger.info(f"Loading model from file: {model_file}")
 
         # Load the model from the file
-        with open(model_file, "rb") as file:
-            self.model = pickle.load(file)
+        try:
+            self.model = load(model_file)
+            self.logger.info(f"Mode ({model_file}) loaded successfully.")
+        except Exception as e:
+            self.logger.error(f"Error occurred while loading model: {e}")
 
     def predict(self):
         """
@@ -111,26 +136,49 @@ class InferModel:
         Returns:
             numpy.ndarray: An array with the model's predictions.
         """
-        predictions = self.model.predict(self.data)
+        self.logger.info("Starting prediction.")
+
+        try:
+            predictions = self.model.predict(self.data)
+            self.logger.info("Prediction completed successfully.")
+        except Exception as e:
+            self.logger.error(f"Error occurred while making prediction: {e}")
+            raise
+
         return predictions
 
     def inverse_scale_predictions(self, predictions):
         """
         Transforms the predictions back to the original scale.
-
-        Args:
-            predictions (numpy.ndarray): An array with
-            the model's predictions.
-
-        Returns:
-            numpy.ndarray: An array with the predictions
-            transformed back to the original scale.
         """
-        predictions = predictions.reshape(-1, 1)
-        return self.target_scaler.inverse_transform(predictions)
+        self.logger.info("Starting inverse scaling of predictions.")
 
-    @staticmethod
-    def save_predictions(result, filename):
+        try:
+            # Reshape the predictions into a 2D array
+            predictions = predictions.reshape(-1, 1)
+
+            inverse_predictions = self.target_scaler.inverse_transform(predictions)
+            self.logger.info("Inverse scaling completed successfully.")
+        except Exception as e:
+            self.logger.error(f"Error occurred while inverse scaling predictions: {e}")
+            raise
+
+        return inverse_predictions
+
+    # @staticmethod
+    # def save_predictions(result, filename):
+    #     """
+    #     Saves the predictions to a .csv file.
+    #
+    #     Args:
+    #         result (pandas.DataFrame): A DataFrame
+    #         with the predictions.
+    #         filename (str): The name of the file
+    #         where the predictions will be saved.
+    #     """
+    #     result.to_csv(filename, index=False)
+
+    def save_predictions(self, result, filename):
         """
         Saves the predictions to a .csv file.
 
@@ -140,10 +188,18 @@ class InferModel:
             filename (str): The name of the file
             where the predictions will be saved.
         """
-        result.to_csv(filename, index=False)
+        self.logger.info(f"Saving predictions to file: {filename}")
+
+        try:
+            result.to_csv(filename, index=False)
+            self.logger.info(f"Predictions saved successfully in {filename} file.")
+        except Exception as e:
+            self.logger.error(f"Error occurred while saving predictions: {e}")
+            raise
 
 
-def main() -> None:
+@hydra.main(config_path="../configs", config_name="config", version_base="1.2")
+def main(cfg: DictConfig) -> None:
     """
     Main entry point of the script.
 
@@ -156,7 +212,7 @@ def main() -> None:
         preprocessed before training.
     """
     # Create an instance of TrainModel with the Hydra configuration
-    model = InferModel()
+    model = InferModel(cfg)
 
     # Load the data
     model.load_data()
--- roman_byelyy_mlops_hw2/package/utils.py
+++ roman_byelyy_mlops_hw2/package/utils.py
@@ -2,6 +2,7 @@
 """
 Useful functions for train and inference operations.
 """
+import logging
 import os
 import subprocess
 
@@ -16,9 +17,9 @@ class OSUtils:
     def __init__(self):
         self.train_file = "train.csv"
         self.test_file = "test.csv"
+        self.logger = logging.getLogger(self.__class__.__name__)
 
-    @staticmethod
-    def import_csv_to_dataframe(file_path):
+    def import_csv_to_dataframe(self, file_path):
         """
         Import a CSV file and convert it to a pandas DataFrame.
 
@@ -28,15 +29,16 @@ class OSUtils:
         Returns:
             df (pd.DataFrame): The loaded data as a pandas DataFrame.
         """
+        self.logger.info(f"Importing CSV file: {file_path}")
         df = pd.read_csv(file_path)
         return df
 
-    @staticmethod
-    def build_file_path(file_name):
+    def build_file_path(self, file_name):
         # Get the current directory
         current_dir = os.path.dirname(os.path.abspath(__file__))
         # Construct the file path
         file_path = os.path.join(current_dir, file_name)
+        self.logger.info(f"Built file path: {file_path}")
         return file_path
 
 
@@ -48,9 +50,9 @@ class DVCUtils:
     def __init__(self):
         self.remote_name = "hw2_dataset"
         self.remote_url = "gdrive://1I_ayLjvj_XboS1YcvcdFIIwiCMTQwYar?hl"
+        self.logger = logging.getLogger(self.__class__.__name__)
 
-    @staticmethod
-    def add_remote_dvc(remote_name, remote_url):
+    def add_remote_dvc(self, remote_name, remote_url):
         """
         Add a new DVC remote.
 
@@ -61,16 +63,13 @@ class DVCUtils:
             remote_name (str): The name of the remote to add.
             remote_url (str): The URL of the remote to add.
         """
-        # Print the name of the remote
-        print(f"DVC Remote to use: {remote_name}")
-
+        self.logger.info(f"Adding DVC remote: {remote_name}")
         # Run the 'dvc remote add' command
         subprocess.run(
             ["dvc", "remote", "add", "-f", remote_name, remote_url], check=True
         )
 
-    @staticmethod
-    def pull_dvc():
+    def pull_dvc(self):
         """
         Pull data from the DVC remote.
 
@@ -78,11 +77,11 @@ class DVCUtils:
         data from the DVC remote. It's useful when
          you want to download data files managed by DVC.
         """
+        self.logger.info("Pulling data from DVC remote.")
         # Run the 'dvc pull' command
         subprocess.run(["dvc", "pull"], check=True)
 
-    @staticmethod
-    def commit_and_push_dvc():
+    def commit_and_push_dvc(self):
         """
         Commit and push changes to the DVC remote.
 
@@ -91,8 +90,99 @@ class DVCUtils:
         and the 'dvc push' command to push any
         changes in the data or pipeline to the DVC remote.
         """
+        self.logger.info("Committing and pushing changes to DVC remote.")
         # Commit changes to DVC files
         subprocess.run(["dvc", "commit"], check=True)
-
         # Push changes to the DVC remote
         subprocess.run(["dvc", "push"], check=True)
+
+
+#
+# class OSUtils:
+#     """
+#     Useful utilities to perform operations: train and inference
+#     """
+#
+#     def __init__(self):
+#         self.train_file = "train.csv"
+#         self.test_file = "test.csv"
+#
+#     @staticmethod
+#     def import_csv_to_dataframe(file_path):
+#         """
+#         Import a CSV file and convert it to a pandas DataFrame.
+#
+#         Args:
+#             file_path (str): The path to the CSV file.
+#
+#         Returns:
+#             df (pd.DataFrame): The loaded data as a pandas DataFrame.
+#         """
+#         df = pd.read_csv(file_path)
+#         return df
+#
+#     @staticmethod
+#     def build_file_path(file_name):
+#         # Get the current directory
+#         current_dir = os.path.dirname(os.path.abspath(__file__))
+#         # Construct the file path
+#         file_path = os.path.join(current_dir, file_name)
+#         return file_path
+#
+#
+# class DVCUtils:
+#     """
+#     Basic operations with DVC repo
+#     """
+#
+#     def __init__(self):
+#         self.remote_name = "hw2_dataset"
+#         self.remote_url = "gdrive://1I_ayLjvj_XboS1YcvcdFIIwiCMTQwYar?hl"
+#
+#     @staticmethod
+#     def add_remote_dvc(remote_name, remote_url):
+#         """
+#         Add a new DVC remote.
+#
+#         This method uses the 'dvc remote add' command
+#         to add a new DVC remote with the given name and URL.
+#
+#         Args:
+#             remote_name (str): The name of the remote to add.
+#             remote_url (str): The URL of the remote to add.
+#         """
+#         # Print the name of the remote
+#         print(f"DVC Remote to use: {remote_name}")
+#
+#         # Run the 'dvc remote add' command
+#         subprocess.run(
+#             ["dvc", "remote", "add", "-f", remote_name, remote_url], check=True
+#         )
+#
+#     @staticmethod
+#     def pull_dvc():
+#         """
+#         Pull data from the DVC remote.
+#
+#         This method uses the 'dvc pull' command to pull
+#         data from the DVC remote. It's useful when
+#          you want to download data files managed by DVC.
+#         """
+#         # Run the 'dvc pull' command
+#         subprocess.run(["dvc", "pull"], check=True)
+#
+#     @staticmethod
+#     def commit_and_push_dvc():
+#         """
+#         Commit and push changes to the DVC remote.
+#
+#         This method uses the 'dvc commit' command to
+#         save changes to DVC files,
+#         and the 'dvc push' command to push any
+#         changes in the data or pipeline to the DVC remote.
+#         """
+#         # Commit changes to DVC files
+#         subprocess.run(["dvc", "commit"], check=True)
+#
+#         # Push changes to the DVC remote
+#         subprocess.run(["dvc", "push"], check=True)
